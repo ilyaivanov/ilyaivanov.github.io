42b9d418184b90d3424239048a629b49eaa980b1
Ilya <illiai@wix.com>
2019-08-19 16:54:11 +0300

    Add Gatsby basic skeleton with routing

---
+++ src/pages/contact.js
@@ -0,0 +1,8 @@
+import React from "react"
+import { Link } from "gatsby"
+export default () => (
+  <div style={{ color: `teal` }}>
+    <Link to="/">Home</Link>
+    <p>Send us a message!</p>
+  </div>
+)
---
+++ src/pages/index.js
@@ -0,0 +1,9 @@
+import React from "react"
+import { Link } from "gatsby"
+
+export default () => (
+  <div style={{ color: "aqua" }}>
+    <Link to="/contact">Contact</Link>
+    <p>Hello world 123!</p>
+  </div>
+)
